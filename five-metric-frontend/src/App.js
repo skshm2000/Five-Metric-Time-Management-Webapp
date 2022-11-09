b84a76a16a09ba699605fa954ca982dabddfd01e
Janhaviprabhu <januprabhu26@gmail.com>
2022-11-09 20:05:28 +0530

    Work page done

--- five-metric-frontend/src/App.js
+++ five-metric-frontend/src/App.js
@@ -1,10 +1,14 @@
 
 import "./App.css";
+import { Task } from "./JPpages/Task";
 import { Time } from "./JPpages/Time";
+import { Work } from "./JPpages/Work";
 
 function App() {
   return <div className="App">
-    
+    {/* <Time/> */}
+     <Task/>
+    {/* <Work/> */}
   </div>;
 }
 
--- five-metric-frontend/src/JPpages/Task.jsx
+++ five-metric-frontend/src/JPpages/Task.jsx
@@ -1,3 +1,33 @@
+import { Box, Button, Flex, Menu, MenuButton, MenuItem, MenuList, Text } from "@chakra-ui/react"
+import { BsChevronDown } from "react-icons/bs";
+
 export const Task=()=>{
-    
+    return (
+      <>
+        <Box>
+          <Flex>
+            <Text>My Task</Text>
+            <Text>|</Text>
+            <Box>
+              <Menu>
+                <MenuButton
+                  color={"grey"}
+                  as={Button}
+                  rightIcon={<BsChevronDown />}
+                >
+                  Select User or Team
+                </MenuButton>
+                <MenuList>
+                  <MenuItem>Download</MenuItem>
+                  <MenuItem>Create a Copy</MenuItem>
+                  <MenuItem>Mark as Draft</MenuItem>
+                  <MenuItem>Delete</MenuItem>
+                </MenuList>
+              </Menu>
+            </Box>
+          </Flex>
+        </Box>
+      </>
+    );
+
 }
