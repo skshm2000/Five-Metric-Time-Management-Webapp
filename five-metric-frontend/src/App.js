31cc7c7d0bf3b6f25609a51fb8d14af6c90c3a9a
Janhaviprabhu <januprabhu26@gmail.com>
2022-11-11 11:20:42 +0530

    updated

--- five-metric-frontend/src/App.js
+++ five-metric-frontend/src/App.js
@@ -1,8 +1,6 @@
 
 import "./App.css";
-import { Task } from "./JPpages/Task";
-import { Time } from "./JPpages/Time";
-import { Work } from "./JPpages/Work";
+
 
 function App() {
   return <div className="App">
