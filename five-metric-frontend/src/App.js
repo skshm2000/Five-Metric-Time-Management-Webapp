1347b6c471a964dc22df662fc29c68ddb4a975c1
Janhaviprabhu <januprabhu26@gmail.com>
2022-11-11 17:04:01 +0530

    pages done with adding side bar

--- five-metric-frontend/src/App.js
+++ five-metric-frontend/src/App.js
@@ -4,12 +4,20 @@ import Signup from './Component-rek/Signup';
 import Login from './Component-rek/Login';
 import Pricing from './Routes/Pricing';
 
+import { Time } from './JPpages/Time';
+import SideBarTrackingComponent from './JPComponents/SideBarTrackingComponent';
+import { Box, Flex } from '@chakra-ui/react';
+import { Task } from './JPpages/Task';
+import { Work } from './JPpages/Work';
+
 function App() {
   return (
     <div className="App">
-      <Pricing/>
+      {/* <Task/> */}
+      {/* <Pricing/>
       <Signup/>
-      <Login/>
+      <Login/> */}
+      <Work/>
     </div>
   );
 }
