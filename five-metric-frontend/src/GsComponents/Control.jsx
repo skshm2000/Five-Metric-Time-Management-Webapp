507efbebd9804ab1fffc7894ee94087ffe80079d
GAURAV SUDHANSHU <gauravsud3@gmail.com>
2022-11-11 23:22:19 +0530

    day-2

---
+++ five-metric-frontend/src/GsComponents/Control.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Control.css"
+const Control = () => {
+  return (
+    <div className='control'>
+      <div>
+        <h1></h1>
+        <ul>
+            <li>
+                
+            </li>
+        </ul>
+      </div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Control
--- five-metric-frontend/src/GsPages/Home.jsx
+++ five-metric-frontend/src/GsPages/Home.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import Bottom from '../GsComponents/Bottom';
 import Footer from '../GsComponents/Footer';
+import MyTime from '../GsComponents/MyTime';
 import Navbar from '../GsComponents/Navbar';
-
+import Features from '../GsComponents/Features';
 const Home = () => {
   return (
-    <div>
-      <Navbar />
-
+    <>
+      <Navbar /><br /><br /><br /><br /><br />
+      <MyTime />
+      <Features />
       <Footer />
-
-    </div>
+      <Bottom />
+    </>
+      
   )
 }
 
