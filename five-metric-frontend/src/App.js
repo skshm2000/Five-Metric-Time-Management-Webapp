16e23135afeb95253dcf4e5fe988deb5fe89b424
rushi6457 <rushiypatil@gmail.com>
2022-11-12 10:03:37 +0530

    add

--- five-metric-frontend/src/App.js
+++ five-metric-frontend/src/App.js
@@ -4,7 +4,6 @@ import Signup from './Component-rek/Signup';
 import Login from './Component-rek/Login';
 import Pricing from './Routes/Pricing';
 import { TimeBar } from './JPComponents/timebar';
-
 import { Time } from './JPpages/Time';
 import SideBarTrackingComponent from './JPComponents/SideBarTrackingComponent';
 import { Box, Flex } from '@chakra-ui/react';
@@ -14,18 +13,11 @@ import { Work } from './JPpages/Work';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      {/* <Pricing/>
+      {/* <Task/> 
+       <Pricing/>
       <Signup/>
-      <Login/> */}
-      <TimeBar />
-=======
-      {/* <Task/> */}
-      {/* <Pricing/>
-      <Signup/>
-      <Login/> */}
-      <Work/>
->>>>>>> 10ef4b52c5f560d3d58b5a99a1c393ac2def59e1
+      <Login/>
+      <Work/> */}
     </div>
   );
 }
